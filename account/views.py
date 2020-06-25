d5ffa6c51d1795b9b6fa147920d44d104fd3d130
bmoyeon <bmoyeon.d@gmail.com>
2020-06-25 18:07:47 +0900

    [refactor] account, guest views

--- account/views.py
+++ account/views.py
@@ -10,7 +10,10 @@ from django.http import (
 from django.db import IntegrityError
 from django.core.exceptions import ValidationError
 
-from ami.settings import SECRET_KEY, ALGORITHM	
+from ami.settings import (
+	SECRET_KEY,
+	ALGORITHM
+)
 from .models import (
 	Account, 
 	Guest
@@ -21,20 +24,19 @@ class AccountSignUpView(View):
 		data = json.loads(request.body)
 		try:
 			if Account.objects.filter(email=data['email']).exists():
-				return JsonResponse({'message':'EMAIL_EXIST'}, status=401)
-			else:
-				hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
-				Account.objects.create(
-					first_name = data['first_name'],
-					last_name  = data['last_name'],
-					email 	   = data['email'],
-					password   = hashed_password.decode('utf-8')
-				)
-				return HttpResponse(status=200)
-		except IntegrityError:
-			return JsonResponse({'message': 'DUPLICATED_EMAIL'}, status = 400)
+				return JsonResponse({'message':'EXISTED_EMAIL'}, status=401)
+			hashed_password = bcrypt.hashpw(data['password'].encode('utf-8'), bcrypt.gensalt())
+			Account.objects.create(
+				first_name = data['first_name'],
+				last_name  = data['last_name'],
+				email 	   = data['email'],
+				password   = hashed_password.decode('utf-8')
+			)
+			return HttpResponse(status=200)
+
 		except ValidationError:
 			return JsonResponse({'message': 'INVALID_EMAIL'}, status = 400)
+
 		except KeyError:
 			return JsonResponse({'message':'INVALID_KEY'}, status=400)
 
@@ -46,10 +48,12 @@ class AccountSignInView(View):
 				user = Account.objects.get(email=data['email'])
 				if bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
 					access_token = jwt.encode({'id':user.id}, SECRET_KEY, ALGORITHM)
+				
 					return JsonResponse({'access_token':access_token.decode('utf-8')}, status=200)
+				
 				return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
-			else:
-				return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
+			return JsonResponse({'message':'UNAUTHORIZED'}, status=401)
+		
 		except KeyError:
 			return JsonResponse({'message':'INVALID_KEY'}, status=400)
 
@@ -59,9 +63,9 @@ class GuestSignUpView(View):
 		try:
 			Guest.objects.create(email = data['email'])
 			return HttpResponse(status=200)
-		except IntegrityError:
-			return JsonResponse({'message': 'DUPLICATED_EMAIL'}, status=400)
+		
 		except ValidationError:
 			return JsonResponse({'message': 'INVALID_EMAIL'}, status=400)
+		
 		except KeyError:
 			return JsonResponse({'message':'INVALID_KEY'}, status=400)
